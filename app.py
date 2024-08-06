0c874be361afa6aa9de22d2fe44ce11347fb7803
zezs <my.version.ctrl@gmail.com>
2024-08-06 23:33:36 +0200

    Integrating frontend and db connection

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from dotenv import load_dotenv
+from langchain_community.utilities import SQLDatabase
 import streamlit as st
 
 load_dotenv()
+
+def init_database(user: str, password: str, host: str, port: str, database: str) -> SQLDatabase:
+    # connceting to mysql db using mysql-connector-python  driver
+    db_uri = f"mysql+mysqlconnector://{user}:{password}@{host}:{port}/{database}"
+    return SQLDatabase.from_uri(db_uri)
+
 
 st.set_page_config(page_title="Chat with SQL", page_icon=":speech_ballon")
 st.title("Chat with my MySQL")
@@ -11,12 +18,22 @@ with st.sidebar:
     st.write("This is a simple chat application using LLM and MySQL")
     st.write("Connect to the databse and satrt chatting.")
 
-    st.text_input("Host", value="localhost")
-    st.text_input("Port", value="3306")
-    st.text_input("User", value="root")
-    st.text_input("Password", type="password", value="admin")
-    st.text_input("Database", value="World")
+    st.text_input("Host", value="localhost", key="Host")
+    st.text_input("Port", value="3306", key="Port")
+    st.text_input("User", value="root", key="User")
+    st.text_input("Password", type="password", value="admin", key="Password")
+    st.text_input("Database", value="World", key="Database")
 
-    st.button("Connect")
+    if st.button("Connect"):
+        with st.spinner("Connecting to database..."):
+            db = init_database(
+                st.session_state["User"],
+                st.session_state["Password"],
+                st.session_state["Host"],
+                st.session_state["Port"],
+                st.session_state["Database"],
+            )
+            st.session_state.db = db
+            st.success("Connected to database!")
 
 st.chat_input("Type a message...")
